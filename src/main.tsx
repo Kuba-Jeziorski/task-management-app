6712514ecb30eb1dac180a92c8589cb51888c79b
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2026-01-26 20:47:12 +0100

    PWA (partially)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { registerSW } from "virtual:pwa-register";
 
 import App from "./App.tsx";
 import { ErrorFallback } from "./components/ui/error-fallback.tsx";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App ready to work offline");
+  },
+});
 
 let root = document.getElementById("root");
 
@@ -21,5 +33,5 @@ createRoot(root).render(
     >
       <App />
     </ErrorBoundary>
-  </StrictMode>
+  </StrictMode>,
 );
